948483e0076f00f3b9c598733cb0d4fabf2bd0f4
Jung Oh <Jung3o@yahoo.com>
2013-09-27 12:45:55 -0500

    did work

--- app/js/localStorage.js
+++ app/js/localStorage.js
@@ -15,10 +15,10 @@ $(function() {
                     +'<li class="message"><h4>'+v['title']+'</h4><p>'+v['extra']+'</p></li></ul>'
                 );
             } else {
-                objTime[k] = getTimePass(v['disabled'] - v['time']);
+                timePass = getTimePass(v['disabled'] - v['time']);
                 $('.doForgetList').append(
                     '<ul class="doForgetTodo urgency-'+v['urgency']+'-done" id="'+k+'">'
-                    +'<li class="timer"><div>'+objTime[k]['day']+' Day</div><div>'+objTime[k]['hour']+' Hour</div><div>'+objTime[k]['minute']+' Min</div><div>'+objTime[k]['second']+' Sec</div></li>'
+                    +'<li class="timer"><div>'+timePass['day']+' Day</div><div>'+timePass['hour']+' Hour</div><div>'+timePass['minute']+' Min</div><div>'+timePass['second']+' Sec</div></li>'
                     +'<li class="message"><h4>'+v['title']+'</h4><p>'+v['extra']+'</p></li></ul>'
                 );
             }
